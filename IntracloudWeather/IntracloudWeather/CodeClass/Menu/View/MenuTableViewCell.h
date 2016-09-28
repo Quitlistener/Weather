330f4b1529d0f65be9108120558454d5755283a0
lanou <lan@Chi66deMac-mini.local>
2016-09-28 22:02:00 +0800

    Menu ViewCell

---
+++ IntracloudWeather/IntracloudWeather/CodeClass/Menu/View/MenuTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  MenuTableViewCell.h
+//  IntracloudWeather
+//
+//  Created by lanou on 2016/9/28.
+//  Copyright © 2016年 guangjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *XYImageView;
+@property (weak, nonatomic) IBOutlet UILabel *XYTitleLabel;
+
+@end
---
+++ IntracloudWeather/IntracloudWeather/CodeClass/Menu/View/MenuTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  MenuTableViewCell.m
+//  IntracloudWeather
+//
+//  Created by lanou on 2016/9/28.
+//  Copyright © 2016年 guangjia. All rights reserved.
+//
+
+#import "MenuTableViewCell.h"
+
+@implementation MenuTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
