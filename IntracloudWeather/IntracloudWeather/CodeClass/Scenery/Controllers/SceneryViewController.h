a420e42569ecce1f122f039cace62f3d06cc2d34
lanou <lan@xiaoqideMac-mini.local>
2016-09-29 21:03:08 +0800

    在tools中拉了瀑布流的方法,搭建瀑布流页面

---
+++ IntracloudWeather/IntracloudWeather/CodeClass/Scenery/Controllers/SceneryViewController.h
@@ -0,0 +1,13 @@
+//
+//  SceneryViewController.h
+//  IntracloudWeather
+//
+//  Created by lanou on 2016/9/29.
+//  Copyright © 2016年 guangjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneryViewController : UIViewController
+
+@end
---
+++ IntracloudWeather/IntracloudWeather/CodeClass/Scenery/Views/FallsCollectionViewCell.m
@@ -0,0 +1,18 @@
+//
+//  FallsCollectionViewCell.m
+//  IntracloudWeather
+//
+//  Created by lanou on 2016/9/29.
+//  Copyright © 2016年 guangjia. All rights reserved.
+//
+
+#import "FallsCollectionViewCell.h"
+
+@implementation FallsCollectionViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
