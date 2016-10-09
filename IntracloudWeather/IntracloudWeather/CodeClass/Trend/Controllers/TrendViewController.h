a8448abd2fed6310118f6fefd794188cf7215647
lanou <lan@xiaoqideMac-mini.local>
2016-10-09 14:02:04 +0800

    趋势模块

---
+++ IntracloudWeather/IntracloudWeather/CodeClass/Trend/Controllers/TrendViewController.h
@@ -0,0 +1,13 @@
+//
+//  TrendViewController.h
+//  IntracloudWeather
+//
+//  Created by lanou on 2016/10/9.
+//  Copyright © 2016年 guangjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrendViewController : UIViewController
+
+@end
