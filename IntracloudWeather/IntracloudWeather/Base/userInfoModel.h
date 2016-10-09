6d704a78db4ec7fd17c341a35502eff9c03a7d6c
lanou <lan@Chi66deMac-mini.local>
2016-10-09 09:21:03 +0800

    userinfo

---
+++ IntracloudWeather/IntracloudWeather/Base/userInfoModel.h
@@ -0,0 +1,14 @@
+//
+//  userInfoModel.h
+//  IntracloudWeather
+//
+//  Created by lanou on 2016/10/9.
+//  Copyright © 2016年 guangjia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface userInfoModel : NSObject
+@property(nonatomic,strong)NSString *cityInfoIdentifier;
+@property(nonatomic,strong)NSString *voiceAI;
+@end
---
+++ IntracloudWeather/IntracloudWeather/Base/userInfoModel.m
@@ -0,0 +1,15 @@
+//
+//  userInfoModel.m
+//  IntracloudWeather
+//
+//  Created by lanou on 2016/10/9.
+//  Copyright © 2016年 guangjia. All rights reserved.
+//
+
+#import "userInfoModel.h"
+
+@implementation userInfoModel
+-(void)setValue:(id)value forUndefinedKey:(NSString *)key{
+    
+}
+@end
