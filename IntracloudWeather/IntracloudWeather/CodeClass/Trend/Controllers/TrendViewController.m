e78fb8ec26cfb67f67009c67e54b1c08f6a1f650
lanou <lan@xiaoqideMac-mini.local>
2016-10-09 14:02:34 +0800

    ...

--- IntracloudWeather/IntracloudWeather/CodeClass/Trend/Controllers/TrendViewController.m
+++ IntracloudWeather/IntracloudWeather/CodeClass/Trend/Controllers/TrendViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "TrendViewController.h"
+#import "JHLineChart.h"
 
 @interface TrendViewController ()
 
@@ -17,6 +18,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    [self initUI];
+    
+}
+
+-(void)initUI{
+    /** 创建表对象 */
+    JHLineChart *lineChart = [[JHLineChart alloc]initWithFrame:CGRectMake(10, 100, SCREEN_width-20, SCREENH_height/2) andLineChartType:JHChartLineValueNotForEveryX];
+    lineChart.backgroundColor = [UIColor clearColor];
+    /* X轴的刻度值 可以传入NSString或NSNumber类型  并且数据结构随折线图类型变化而变化 详情看文档或其他象限X轴数据源示例*/
+    lineChart.xLineDataArr = @[@"周日",@"周一",@"周二",@"周三",@"周四",@"周五",@"周六",@0];
+    /* 折线图的不同类型  按照象限划分 不同象限对应不同X轴刻度数据源和不同的值数据源 */
+    lineChart.lineChartQuadrantType = JHLineChartQuadrantTypeFirstQuardrant;
+    /* 数据源 */
+    lineChart.valueArr = @[@[@"38",@"35",@"32",@37,@30,@32,@35],@[@25,@19,@22,@23]];
+    /* 值折线的折线颜色 默认暗黑色*/
+    lineChart.valueLineColorArr =@[ [UIColor purpleColor], [UIColor brownColor]];
+    /* 值点的颜色 默认橘黄色*/
+    lineChart.pointColorArr = @[[UIColor orangeColor],[UIColor yellowColor]];
+    /* X和Y轴的颜色 默认暗黑色 */
+    lineChart.xAndYLineColor = [UIColor blackColor];
+    /* XY轴的刻度颜色 m */
+    lineChart.xAndYNumberColor = [UIColor blueColor];
+    /*        设置是否填充内容 默认为否         */
+    lineChart.contentFill = NO;
+    /*        设置为曲线路径         */
+    lineChart.pathCurve = NO;
+    [self.view addSubview:lineChart];
+    /*        开始动画         */
+    [lineChart showAnimation];
 }
 
 - (void)didReceiveMemoryWarning {
