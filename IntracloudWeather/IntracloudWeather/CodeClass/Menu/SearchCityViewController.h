581c7f4ae122689fff938b708f86522858d5fb99
lanou <lan@Chi66deMac-mini.local>
2016-10-10 20:12:34 +0800

    push

---
+++ IntracloudWeather/IntracloudWeather/CodeClass/Menu/SearchCityViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchCityViewController.h
+//  IntracloudWeather
+//
+//  Created by lanou on 2016/10/10.
+//  Copyright © 2016年 guangjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchCityViewController : UIViewController
+
+@end
